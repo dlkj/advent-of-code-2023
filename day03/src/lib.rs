ce1c75b88c3f6be7ae0b31b954a1423d1479de74
Daniel Kenyon-Jones <github@dlkj.co.uk>
2023-12-03 15:33:48 +0000

    day03b

--- day03/src/lib.rs
+++ day03/src/lib.rs
@@ -30,8 +30,8 @@ fn parse_line(input: &mut Located<&str>) -> PResult<Vec<(Token, Range<usize>)>> 
         delimited(
             parse_dots,
             alt((
-                dec_uint.with_span().map(|(n, r)| (Token::Number(n), r)),
-                any.with_span().map(|(c, r)| (Token::Symbol(c), r)),
+                dec_uint.map(Token::Number).with_span(),
+                any.map(Token::Symbol).with_span(),
             )),
             parse_dots,
         ),
@@ -47,7 +47,7 @@ pub fn solve_a(input: &str) -> u32 {
         .enumerate()
         .collect_vec();
 
-    let symbol_cords = rows
+    let symbol_coordinates = rows
         .iter()
         .flat_map(|(y, row)| {
             row.iter().filter_map(|(t, r)| {
@@ -74,9 +74,11 @@ pub fn solve_a(input: &str) -> u32 {
                         end: y.saturating_add(2), //range is exclusive above
                     };
 
-                    symbol_cords
+                    symbol_coordinates
                         .iter()
-                        .any(|(x, y)| x_range.contains(x) && y_range.contains(y))
+                        .any(|(symbol_x, symbol_y)| {
+                            x_range.contains(symbol_x) && y_range.contains(symbol_y)
+                        })
                         .then_some(n)
                 } else {
                     None
@@ -88,16 +90,56 @@ pub fn solve_a(input: &str) -> u32 {
 
 #[must_use]
 pub fn solve_b(input: &str) -> u32 {
-    todo!()
+    let rows = input
+        .lines()
+        .map(|line| parse_line.parse(Located::new(line)).unwrap())
+        .enumerate()
+        .collect_vec();
+
+    let number_areas = rows
+        .iter()
+        .flat_map(|(y, row)| {
+            row.iter().filter_map(|(t, r)| {
+                if let Token::Number(n) = t {
+                    Some((
+                        *n,
+                        Range {
+                            start: r.start.saturating_sub(1),
+                            end: r.end.saturating_add(1),
+                        },
+                        Range {
+                            start: y.saturating_sub(1),
+                            end: y.saturating_add(2), //range is exclusive above
+                        },
+                    ))
+                } else {
+                    None
+                }
+            })
+        })
+        .collect_vec();
+
+    rows.iter()
+        .flat_map(|(y, row)| {
+            row.iter().filter_map(|(t, r)| match t {
+                Token::Symbol('*') => number_areas
+                    .iter()
+                    .filter_map(|(n, num_x, num_y)| {
+                        (num_x.contains(&r.start) && num_y.contains(y)).then_some(n)
+                    })
+                    .collect_tuple()
+                    .map(|(a, b)| a * b),
+                _ => None,
+            })
+        })
+        .sum()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn example_a() {
-        let input = "467..114..
+    const INPUT: &str = "467..114..
 ...*......
 ..35..633.
 ......#...
@@ -108,14 +150,14 @@ mod tests {
 ...$.*....
 .664.598..";
 
-        assert_eq!(solve_a(input), 4361);
+    #[test]
+    fn example_a() {
+        assert_eq!(solve_a(INPUT), 4361);
     }
 
     #[test]
     fn example_b() {
-        let input = "";
-
-        assert_eq!(solve_b(input), 0);
+        assert_eq!(solve_b(INPUT), 467_835);
     }
 
     #[test]
@@ -125,6 +167,6 @@ mod tests {
 
     #[test]
     fn b() {
-        assert_eq!(solve_b(include_str!("input.txt")), 0);
+        assert_eq!(solve_b(include_str!("input.txt")), 87_263_515);
     }
 }
