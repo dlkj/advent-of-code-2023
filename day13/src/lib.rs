3107280ac01d8321f2e848f21475047dbbebaf42
Daniel Kenyon-Jones <github@dlkj.co.uk>
2023-12-15 17:28:45 +0000

    day13b

--- day13/src/lib.rs
+++ day13/src/lib.rs
@@ -65,7 +65,70 @@ fn find_mirror_rows(rows: &[String]) -> Option<usize> {
 
 #[must_use]
 pub fn solve_b(input: &str) -> usize {
-    input.len()
+    let maps = parse_maps.parse(input).unwrap();
+
+    maps.into_iter()
+        .map(|m| {
+            if let Some(value) = find_smudge_rows(&m) {
+                return (value + 1) * 100;
+            }
+
+            // transpose
+            let mut transpose: Vec<String> = vec![];
+            for s in &m {
+                for (x, c) in s.chars().enumerate() {
+                    if let Some(s) = transpose.get_mut(x) {
+                        s.push(c);
+                    } else {
+                        transpose.push(c.to_string());
+                    }
+                }
+            }
+
+            // try again
+            if let Some(value) = find_smudge_rows(&transpose) {
+                return value + 1;
+            }
+            panic!("no mirror found for {m:?}")
+        })
+        .sum()
+}
+
+fn find_smudge_rows(rows: &[String]) -> Option<usize> {
+    (0..(rows.len() - 1)).find(|&i| {
+        let mut single_error = false;
+        (0..=i).all(|j| {
+            let mirror_row = i * 2 - j + 1;
+            if let Some(right) = rows.get(mirror_row) {
+                if right == &rows[j] {
+                    true
+                } else if !single_error && single_difference(right, &rows[j]) {
+                    single_error = true;
+                    true
+                } else {
+                    false
+                }
+            } else {
+                true
+            }
+        }) && single_error
+    })
+}
+
+fn single_difference(right: &str, left: &str) -> bool {
+    if right.len() != left.len() {
+        return false;
+    }
+
+    let mut single_error = false;
+    for (r, l) in right.chars().zip(left.chars()) {
+        if r != l && !single_error {
+            single_error = true;
+        } else if r != l && single_error {
+            return false;
+        }
+    }
+    single_error
 }
 
 #[cfg(test)]
@@ -95,7 +158,7 @@ mod tests {
 
     #[test]
     fn example_b() {
-        assert_eq!(solve_b(INPUT), 0);
+        assert_eq!(solve_b(INPUT), 400);
     }
 
     #[test]
@@ -105,6 +168,6 @@ mod tests {
 
     #[test]
     fn solution_b() {
-        assert_eq!(solve_b(include_str!("input.txt")), 0);
+        assert_eq!(solve_b(include_str!("input.txt")), 31_108);
     }
 }
